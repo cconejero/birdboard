ca9ec0140f0767571353105e225e644f30182e34
Claudio Conejero <conejero.claudio@gmail.com>
2022-07-10 19:16:14 -0300

    Episode 5: A Project Requires An Owner

--- database/migrations/2022_07_03_014612_create_projects_table.php
+++ database/migrations/2022_07_03_014612_create_projects_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('owner_id')->references('id')->on('users')->onDelete('cascade');
             $table->string('title');
             $table->string('description');
             $table->timestamps();
--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -12,15 +12,25 @@ class ProjectsTest extends TestCase
 
     use WithFaker, RefreshDatabase;
 
+
     /** @test */
-    public function a_user_can_create_a_project()
-    {
+    public function only_authenticated_users_can_create_projects(){
+
+        $attributes = Project::factory()->raw(['owner_id' => null]);
+
+        $this->post('/projects', $attributes)->assertRedirect('login');
+    }
+
+    /** @test */
+    public function a_user_can_create_a_project(){
 
         $this->withoutExceptionHandling();
 
+        $this->actingAs(\App\Models\User::create());
+
         $attributes = [
             'title' => $this->faker->sentence,
-            'description' => $this->faker->paragraph
+            'description' => $this->faker->paragraph,
         ];
 
         $this->post('/projects', $attributes)->assertRedirect('/projects');
@@ -33,8 +43,6 @@ class ProjectsTest extends TestCase
     /** @test  */
     public function a_user_can_view_a_project()
     {
-        $this->withoutExceptionHandling();
-
         $project = Project::factory()->create();
 
         $this->get($project->path())
@@ -45,6 +53,8 @@ class ProjectsTest extends TestCase
     /** @test */
     public function a_project_requires_a_title(){
 
+        $this->actingAs(\App\Models\User::create());
+
         $attributes = Project::factory()->raw(['title' => '']);
 
         $this->post('/projects', $attributes)->assertSessionHasErrors('title');
@@ -53,8 +63,11 @@ class ProjectsTest extends TestCase
     /** @test */
     public function a_project_requires_a_description(){
 
+        $this->actingAs(\App\Models\User::create());
+
         $attributes = Project::factory()->raw(['description' => '']);
 
         $this->post('/projects', $attributes)->assertSessionHasErrors('description');
     }
+
 }
