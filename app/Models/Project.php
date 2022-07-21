14123a10cab1492b01fe6bd7c6302e887636bca2
Claudio Conejero <conejero.claudio@gmail.com>
2022-07-21 02:51:10 -0300

    Episode 20: Project activity feeds

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -43,4 +43,9 @@ class Project extends Model
     {
         return $this->tasks()->create(compact('body'));
     }
+
+    public function activity()
+    {
+        return $this->hasMany(Activity::class);
+    }
 }
