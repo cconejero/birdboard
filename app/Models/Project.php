f437fa34ec9d484ab9d203650c54777d5536dea7
Claudio Conejero <conejero.claudio@gmail.com>
2022-07-21 22:41:59 -0300

    Episode 26: Recording Model Changes.

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,12 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
 
 class Project extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    public array $old = [];
 
     /**
      * @return string
@@ -57,6 +60,24 @@ class Project extends Model
      */
     public function recordActivity($description)
     {
-        $this->activity()->create(['description' => $description]);
+        $this->activity()->create([
+            'description' => $description,
+            'changes' => $this->activityChanges($description),
+        ]);
+    }
+
+    /**
+     * @return array
+     */
+    public function activityChanges($description)
+    {
+        if ($description === 'updated') {
+            return [
+                'before' => Arr::except(array_diff($this->old, $this->getAttributes()), 'updated_at'),
+                'after' => Arr::except($this->getChanges(), 'updated_at'),
+            ];
+        } else {
+            return null;
+        }
     }
 }
